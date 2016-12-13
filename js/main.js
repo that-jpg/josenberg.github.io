ebc19cfac3800dfa6f55d9af420352fdb609c069
Josenberg <mbx.philipe@gmail.com>
2016-12-13 14:28:14 -0200

    adding ascii astronaut

--- js/main.js
+++ js/main.js
@@ -32,4 +32,19 @@ $("#banner").append(header.canvas());
   });
 
   $("#footer").append(footer.canvas());
+
+console.log(" %c Do not go gentle into that good night",  "font-weight: bold;");
+console.log(`Good men, the last wave by, crying how bright
+Their frail deeds might have danced in a green bay,
+Rage, rage against the dying of the light.`);
+console.log("               +  .        _            .-.\n" +
+"                       3==({)_    .    (   \\ \n" +
+"      +  .             _  )`\-|      *  )   \\ \n" +
+"              .       /_`' // |\     .-'     `-a:f \n" +
+"    .                B'/`-'M\_| )   /       . \n" +
+"            . *       //       (   /      . \n" +
+"                     B'         `-' \n");
+console.log(" %c Tem algum projeto interessante? Entre em contato: mbx.philipe@gmail.com",  "font-weight: bold;");
+
+
 })();
